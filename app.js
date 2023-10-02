3f920b4f34b907b7e5579c8166c8b100efc28d17
ayush21je0217 <134034042+ayush21je0217@users.noreply.github.com>
2023-10-02 16:26:23 +0530

    Update app.js
    previouly it was not working so had to do some research and finally implemented the find function with the try catch block

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const { findById } = require("mongoose/lib/model");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,8 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1");
-//changing it to a local database
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1",{ useUnifiedTopology: true },{ useNewUrlParser: true });
 
 const postSchema = {
   title: String,
@@ -25,13 +27,14 @@ const postSchema = {
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
-
-  Post.find({}, function(err, posts){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
-      });
-  });
+ Post.find({"content":{$ne:null}}).then((found)=>{
+  if(found)
+  {
+    res.render("home",{startingContent:homeStartingContent,posts:found});
+  }
+ }).catch((err)=>{
+  console.log(err);
+ })
 });
 
 app.get("/compose", function(req, res){
@@ -39,30 +42,33 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = new Post({
+  let post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
-
-
-  post.save(function(err){
-    if (!err){
-        res.redirect("/");
+  post.save();
+  Post.find().then((found)=>{
+    if(found)
+    {
+      res.render("home",{startingContent:homeStartingContent,posts:found});
     }
-  });
+   }).catch((err)=>{
+    console.log(err);
+   })
 });
 
 app.get("/posts/:postId", function(req, res){
 
 const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
-  });
-
+  Post.findOne({_id: requestedPostId}).then((found)=>{
+    if(found)
+    {
+      res.render("post",{title:found.title,
+      content:found.content});
+    }
+  }).catch((err)=>{
+    console.log(err);
+  })
 });
 
 app.get("/about", function(req, res){
@@ -72,8 +78,6 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
